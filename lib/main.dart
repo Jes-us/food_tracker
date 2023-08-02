994572badb516e6434f2d27135fc189b598e1ff3
Jesus Rodriguez <jermadrid1984@gmail.com>
2023-08-01 20:08:58 -0600

    Adding the code for the experiment to read a barcode

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'theme.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter/services.dart';
 
 ThemeMode customizedThemeMode = ThemeMode.dark;
 
@@ -52,6 +54,30 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  String _scanBarcode = 'Unknown';
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print('codigo de barras');
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Consumer<Manage>(
       builder: (context, Manage themeNotifier, child) {
@@ -105,6 +131,9 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
+              FloatingActionButton(onPressed: () {
+                scanBarcodeNormal();
+              })
             ],
           ),
         );
