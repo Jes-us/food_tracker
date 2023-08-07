98514c64a5d1f4b80e43747c64ba2b6e4b653a34
Jesus Rodriguez <jermadrid1984@gmail.com>
2023-08-07 15:02:36 -0600

    Barcode funtionality

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,10 @@
 import 'package:flutter/material.dart';
 import 'theme.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:flutter/services.dart';
+import 'home/homepage.dart';
+//import 'utilities/barcode_reader.dart';
 
 ThemeMode customizedThemeMode = ThemeMode.dark;
-
-var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 96, 59, 181),
-);
-
-var kDarkColorScheme = ColorScheme.fromSeed(
-  brightness: Brightness.dark,
-  seedColor: const Color.fromARGB(255, 5, 99, 125),
-);
 
 void main() {
   runApp(const MyApp());
@@ -33,10 +24,12 @@ class MyApp extends StatelessWidget {
           builder: (context, Manage themenotifier, child) {
             return MaterialApp(
               theme: ThemeData(
-                  colorScheme: kColorScheme,
-                  appBarTheme: AppBarTheme(color: Colors.blueAccent)),
+                colorScheme: kColorScheme,
+                useMaterial3: false,
+              ),
               darkTheme: ThemeData(
                 colorScheme: kDarkColorScheme,
+                useMaterial3: false,
               ),
               themeMode: themenotifier.getActualTheme(),
               home: HomePage(),
@@ -47,97 +40,4 @@ class MyApp extends StatelessWidget {
 }
 
 // ignore: use_key_in_widget_constructors
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
-  @override
-  String _scanBarcode = 'Unknown';
-
-  Future<void> scanBarcodeNormal() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
-      print('codigo de barras');
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _scanBarcode = barcodeScanRes;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Consumer<Manage>(
-      builder: (context, Manage themeNotifier, child) {
-        return Scaffold(
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    themeNotifier.changeTheme(customizedThemeMode);
-                  },
-                  icon: Icon(Icons.dark_mode_rounded)),
-            ],
-            title: Text('App de prueba para provider'),
-          ),
-          //agreo comentario de prueba
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                      color: Theme.of(context).cardColor,
-                    ),
-                  )),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        width: 10,
-                        height: 20,
-                        color: Theme.of(context).cardColor,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        width: 10,
-                        height: 20,
-                        color: Theme.of(context).cardColor,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              FloatingActionButton(onPressed: () {
-                scanBarcodeNormal();
-              })
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,78 @@
 import 'package:flutter/material.dart';
 
+const kColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF7C5800),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFFFDEA6),
+  onPrimaryContainer: Color(0xFF271900),
+  secondary: Color(0xFF705D00),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFFFE172),
+  onSecondaryContainer: Color(0xFF221B00),
+  tertiary: Color(0xFF00696D),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFF6FF6FD),
+  onTertiaryContainer: Color(0xFF002021),
+  error: Color(0xFFBA1A1A),
+  errorContainer: Color(0xFFFFDAD6),
+  onError: Color(0xFFFFFFFF),
+  onErrorContainer: Color(0xFF410002),
+  background: Color(0xFFFAFCFF),
+  onBackground: Color(0xFF001F2A),
+  surface: Color(0xFFFAFCFF),
+  onSurface: Color(0xFF001F2A),
+  surfaceVariant: Color(0xFFEEE1CF),
+  onSurfaceVariant: Color(0xFF4E4639),
+  outline: Color(0xFF807667),
+  onInverseSurface: Color(0xFFE1F4FF),
+  inverseSurface: Color(0xFF003547),
+  inversePrimary: Color(0xFFFBBC34),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFF7C5800),
+  outlineVariant: Color(0xFFD1C5B4),
+  scrim: Color(0xFF000000),
+);
+
+const kDarkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFFFBBC34),
+  onPrimary: Color(0xFF412D00),
+  primaryContainer: Color(0xFF5E4200),
+  onPrimaryContainer: Color(0xFFFFDEA6),
+  secondary: Color(0xFFE4C54A),
+  onSecondary: Color(0xFF3B2F00),
+  secondaryContainer: Color(0xFF554600),
+  onSecondaryContainer: Color(0xFFFFE172),
+  tertiary: Color(0xFF4CD9E0),
+  onTertiary: Color(0xFF003739),
+  tertiaryContainer: Color(0xFF004F53),
+  onTertiaryContainer: Color(0xFF6FF6FD),
+  error: Color(0xFFFFB4AB),
+  errorContainer: Color(0xFF93000A),
+  onError: Color(0xFF690005),
+  onErrorContainer: Color(0xFFFFDAD6),
+  background: Color(0xFF001F2A),
+  onBackground: Color(0xFFBFE9FF),
+  surface: Color(0xFF001F2A),
+  onSurface: Color(0xFFBFE9FF),
+  surfaceVariant: Color(0xFF4E4639),
+  onSurfaceVariant: Color(0xFFD1C5B4),
+  outline: Color(0xFF9A8F80),
+  onInverseSurface: Color(0xFF001F2A),
+  inverseSurface: Color(0xFFBFE9FF),
+  inversePrimary: Color(0xFF7C5800),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFFFBBC34),
+  outlineVariant: Color(0xFF4E4639),
+  scrim: Color(0xFF000000),
+);
+
+/*ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);*/
+
 class Manage extends ChangeNotifier {
   Manage(this.actualTheme);
 
